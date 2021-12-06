031612fc4ea674801a28d1c9c2d225e7adeb2dc3
Luis <luismartinezmilla@gmail.com>
2021-12-06 22:08:51 +0100

    finished 1st middleware

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use app\Models\User;
+use Exception;
 
 class UsuariosController extends Controller
 {
--- app/Http/Middleware/checkUserProfile.php
+++ app/Http/Middleware/checkUserProfile.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use app\Models\User;
+use Exception;
 
 class checkUserProfile
 {
@@ -18,9 +20,38 @@ class checkUserProfile
     {
         //comprobacion si tiene permisos (si es empleado, RRHH o directivo)
         /*
-        Si es RRHH o directivo return next
+        Si es RRHH o directivo return next y le paso el usuario
         si es empleado return response con un error
         */
-        return $next($user);
+        $response = "";
+        try{
+            $user = User::where('api_token')->first();
+            switch ($user->puesto) {
+                case 'empleado':
+                    $permiso = 1;
+                    break;
+                case 'RRHH':
+                    $permiso = 2;
+                    break;
+                case 'directivo':
+                    $permiso = 3;
+                    break;
+                default:
+                    throw new Exception();
+                    break;
+            }
+            if($permiso>=2){
+                return $next($user,$permiso);
+            }else{
+                $response["status"] = 0;
+                $response["msg"] = "No tienes permisos suficientes";
+            }
+        }catch(\Exception $e){
+            $response["status"] = 0;
+            $response["msg"] = "No has iniciado sesión";
+            $response["Exception"] = $e;
+        }
+        return $response;
+        
     }
 }
